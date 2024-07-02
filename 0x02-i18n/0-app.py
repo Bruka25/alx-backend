f81d50c29cb07ec838e5b7a8ab1ae88c89678110
Bruka25 <bkteshome862@gmail.com>
2024-07-02 22:28:43 +0300

    task 1 and 2 pycodestyle fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
+"""Module for initializing Flask app"""
+
 from flask import Flask, render_template
 
+# Instantiate the Flask application
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
+    """
+    Route for the index page.
+
+    This route renders the '0-index.html' template when accessed.
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
+    # Run the Flask app on host 0.0.0.0 and port 5000
     app.run(host='0.0.0.0', port=5000)
